1361681180bc6c6e246072d3bc9b191218f5b095
rizky14 <madya14.rizky@gmail.com>
2014-12-11 11:44:54 +0700

    update kedua menambahkan output "Kampus Unikom"

--- src/latihanrpl/LatihanRpl.java
+++ src/latihanrpl/LatihanRpl.java
@@ -17,6 +17,7 @@ public class LatihanRpl {
     public static void main(String[] args) {
         // TODO code application logic here
        System.out.println("Selamat Datang di Unikom");
+       System.out.println("Kampus Universitas Komputer Indonesia");
     }
     
 }
